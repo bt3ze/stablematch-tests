7fe0f499368922baf3aef30bd3209559b43fe5ed
Ben Terner <bt3ze@virginia.edu>
2014-02-20 23:23:20 -0500

    working correctly on pequod again.

--- testcircuit/galelimited.c
+++ testcircuit/galelimited.c
@@ -1,14 +1,14 @@
 // global variables needed for the stable matching
-#define NUM_SUITORS 20 // number of suitors and reviewers
-#define NUM_REVIEWERS 20
-#define NIL NUM_SUITORS // MUST be chosen to be the greater of NUM_SUITORS and NUM_REVIEWERS
+#define NUM_SUITORS 50 // number of suitors and reviewers
+#define NUM_REVIEWERS 50
+#define NIL NUM_REVIEWERS // MUST be chosen to be the greater of NUM_SUITORS and NUM_REVIEWERS
 
 // number of potential matches we permit each player to rank of the opposite party before declaring they would rather remain unmatched
-#define K_SUITORS 5
-#define K_REVIEWERS 5
+#define K_SUITORS 20
+#define K_REVIEWERS 20
 
-#define __CPROG__
-//#define __PCFPROG__
+//#define __CPROG__
+#define __PCFPROG__
 
 #ifdef __CPROG__
 #include "ext-input.c"
